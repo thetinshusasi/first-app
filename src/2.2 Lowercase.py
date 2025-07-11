ac31adf2289d55976e14c6be7689ee276ce973e6
thetinshusasi <thetinshusasi@gmail.com>
2025-07-11 08:10:47 -0300

    docs(app): ticket 11 july 2025 - 1 Stop words and lower case letter

--- src/2.2 Lowercase.py
+++ src/2.2 Lowercase.py
@@ -16,3 +16,27 @@ print(long_sentence)
 print(long_sentence.lower())
 
 #%%
+
+sentence_list = [
+    "Hello World",
+    "Goodbye World 1",
+]
+print([sentence.lower() for sentence in sentence_list])
+# %%
+
+
+#%%
+
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+english_stopwords = stopwords.words('english')
+print( len(english_stopwords))
+
+lowercase_sentence  = "The quick brown fox jumps over the lazy dog while the magnificent rainbow".lower()
+print([word for word in lowercase_sentence.split() if word not in english_stopwords])
+
+
+#%%
+# %%
